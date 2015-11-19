df25576b2a94a543215827a6145c1e7b8baa1f73
Mark J. Titorenko <mark.titorenko@alces-software.com>
2015-11-19 23:32:01 +0000

    Initial backends for POSIX and S3 API-compatible storage.

---
+++ posix/posix-storage.functions.sh
@@ -0,0 +1,219 @@
+#==============================================================================
+# Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Clusterware.
+#
+# Alces Clusterware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Clusterware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Clusterware, please visit:
+# https://github.com/alces-software/clusterware
+#==============================================================================
+posix_storage_configure() {
+    local name path system
+    if [ "$1" == "--system" ]; then
+        system="$1"
+        shift
+    fi
+    name="$1"
+    path="$2"
+
+    echo -n "Display name [${name}]: " >/dev/stderr
+    read display_name
+    display_name="${display_name:-${name}}"
+
+    while [ -z "${path}" ]; do
+        echo -n "Path: " >/dev/stderr
+        read path
+    done
+
+    data=$(cat <<EOF
+---
+name: ${name}
+type: posix
+dir: ${path}
+EOF
+    )
+    if ! echo "${data}" | storage_write_configuration ${system} "${name}.target.yml"; then
+        echo "failed to write ${name}.target.yml configuration"
+        return 1
+    fi
+    data=$(cat <<\EOF
+if [ "${cw_POSIX_path:0:1}" == "%" ]; then
+  cw_POSIX_path="$(echo "${cw_POSIX_path:1}" | sed -e "s,#{dir},$HOME,g" -e "s,#{name},$(id -un),g")"
+  cw_POSIX_path="$(echo "${cw_POSIX_path}" | sed -e "s,#{::Dir.tmpdir},${TMPDIR:-/tmp},g")"
+fi
+EOF
+    )
+    if ! echo -e "cw_POSIX_path=\"${path}\"\n${data}" | storage_write_configuration ${system} "${name}.posix.rc"; then
+        echo "failed to write ${name}.posix.rc configuration"
+        return 1
+    fi
+}
+
+posix_storage_sanitize_target() {
+    local target
+    target="$(echo "$1" | sed 's,//*\([^/]\),/\1,g')"
+    if [ "${target:0:1}" == "/" ]; then
+        target="${target:1}"
+    fi
+    while [ "${target:0:3}" == "../" ]; do
+        target="${target:3}"
+    done
+    if [ "${target}" == ".." ]; then
+        target=""
+    fi
+    echo "${target}"
+}
+
+posix_storage_list() {
+    local name target
+    name="$1"
+    target=$(posix_storage_sanitize_target "$2")
+    . $(storage_get_configuration "${name}")
+    (
+        cd "${HOME}"
+        set -o pipefail
+        if find "${cw_POSIX_path}"/"${target}" \
+            -mindepth 1 \
+            -maxdepth 1 \
+            -type d \
+            -printf "%AY-%Am-%Ad %AH:%AM       DIR   %f\n" 2>/dev/null | sort -k4; then
+            find "${cw_POSIX_path}"/"${target}" \
+                -maxdepth 1 \
+                -type f \
+                -printf "%AY-%Am-%Ad %AH:%AM %9s   %f\n" | sort -k4
+        else
+            echo "not found: ${target}"
+        fi
+        set +o pipefail
+    )
+}
+
+posix_storage_put() {
+    local name source target
+    name="$1"
+    source="$(cd "$(dirname "$2")" && pwd)/$(basename "$2")"
+    target="$(posix_storage_sanitize_target "${3:-$(basename "$source")}")"
+    . $(storage_get_configuration "${name}")
+    (
+        cd "${HOME}"
+        mkdir -p "$(dirname "${cw_POSIX_path}"/"${target}")" 2>/dev/null &&
+          cp -p "${source}" "${cw_POSIX_path}"/"${target}"
+    )
+    if [ $? -gt 0 ]; then
+        echo "put to storage failed"
+        return 1
+    else
+        echo "${2} -> ${name}:${target}"
+    fi
+}
+
+posix_storage_get() {
+    local name source target targetdir target_is_dir
+    name="$1"
+    source="$(posix_storage_sanitize_target "$2")"
+    if [ -d "$3" ]; then
+        targetdir="$3"
+        target_is_dir=true
+    else
+        targetdir="$(dirname "${3}")"
+    fi
+    if [ ! -d "${targetdir}" ]; then
+        echo "get from storage failed - no such target directory"
+        return 1
+    fi
+    if [ "$target_is_dir" ]; then
+        target="$(cd "${targetdir}" && pwd)/$(basename "$source")"
+    else
+        target="$(cd "${targetdir}" && pwd)/$(basename "${3:-$source}")"
+    fi
+    . $(storage_get_configuration "${name}")
+    (
+        cd "${HOME}"
+        cp -p "${cw_POSIX_path}"/"${source}" "${target}"
+    )
+    if [ $? -gt 0 ]; then
+        echo "get from storage failed"
+        return 1
+    else
+        echo "${name}:${source} -> ${target}"
+    fi
+}
+
+posix_storage_rm() {
+    local name target
+    name="$1"
+    target="$(posix_storage_sanitize_target "$2")"
+    . $(storage_get_configuration "${name}")
+    (
+        cd "${HOME}"
+        if rm -f "${cw_POSIX_path}"/"${target}" 2>/dev/null; then
+            bucket="${target%%/*}"
+            if [ -d "${cw_POSIX_path}"/"${bucket}" ]; then
+                cd "${cw_POSIX_path}"/"${bucket}"
+                rmdir --ignore-fail-on-non-empty -p "$(dirname "${target#*/}")"
+            fi
+        else
+            exit 1
+        fi
+    )
+    if [ $? -gt 0 ]; then
+        echo "delete from storage failed"
+        return 1
+    else
+        echo "deleted ${name}:${target}"
+    fi
+}
+
+posix_storage_rmbucket() {
+    local name target
+    name="$1"
+    if [[ "$2" == *"/"* ]]; then
+        echo "invalid '/' character in bucket name"
+        return 1
+    fi
+    target="$2"
+    . $(storage_get_configuration "${name}")
+    (
+        cd "${HOME}"
+        rmdir "${cw_POSIX_path}"/"${target}" 2>/dev/null
+    )
+    if [ $? -gt 0 ]; then
+        echo "bucket removal failed"
+        return 1
+    else
+        echo "removed bucket ${name}:${target}"
+    fi
+}
+
+posix_storage_mkbucket() {
+    local name target
+    name="$1"
+    if [[ "$2" == *"/"* ]]; then
+        echo "invalid '/' character in bucket name"
+        return 1
+    fi
+    target="$2"
+    . $(storage_get_configuration "${name}")
+    (
+        cd "${HOME}"
+        mkdir "${cw_POSIX_path}"/"${target}" 2>/dev/null
+    )
+    if [ $? -gt 0 ]; then
+        echo "bucket creation failed"
+        return 1
+    else
+        echo "created bucket ${name}:${target}"
+    fi
+}
---
+++ s3/s3-storage.functions.sh
@@ -0,0 +1,146 @@
+#==============================================================================
+# Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Clusterware.
+#
+# Alces Clusterware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Clusterware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Clusterware, please visit:
+# https://github.com/alces-software/clusterware
+#==============================================================================
+s3_storage_configure() {
+    local name access_key secret_key system
+    if [ "$1" == "--system" ]; then
+        system="$1"
+        shift
+    fi
+    name="$1"
+    access_key="$2"
+
+    echo -n "Display name [${name}]: " >/dev/stderr
+    read display_name
+    display_name="${display_name:-${name}}"
+
+    while [ -z "${access_key}" ]; do
+        echo -n "Access key: " >/dev/stderr
+        read access_key
+    done
+
+    while [ -z "${secret_key}" ]; do
+        echo -n "Secret key: " >/dev/stderr
+        read -s secret_key
+        echo >/dev/stderr
+    done
+
+    echo -n "Service address [s3.amazonaws.com]: " >/dev/stderr
+    read address
+    address="${address:-s3.amazonaws.com}"
+
+    data=$(cat <<EOF
+---
+name: ${name}
+type: s3
+access_key: ${access_key}
+secret_key: ${secret_key}
+address: ${address}
+EOF
+    )
+    if ! echo "${data}" | storage_write_configuration ${system} "${name}.target.yml"; then
+        echo "failed to write ${name}.target.yml configuration"
+        return 1
+    fi
+    data=$(cat <<EOF
+[default]
+access_key = ${access_key}
+secret_key = ${secret_key}
+host_base = ${address}
+host_bucket = %(bucket)s.${address}
+use_https = True
+check_ssl_certificate = True
+EOF
+    )
+    if ! echo "${data}" | storage_write_configuration ${system} "${name}.s3.cfg"; then
+        echo "failed to write ${name}.s3.cfg configuration"
+        return 1
+    fi
+}
+
+s3_storage_perform() {
+    local name cmd
+    cmd="$1"
+    name="$2"
+    shift 2
+    "${cw_ROOT}"/opt/s3cmd/s3cmd -c "$(storage_get_configuration "${name}")" "${cmd}" "$@" 2>&1 | \
+        grep -v 'python-magic'
+}
+
+s3_storage_list() {
+    local args
+    if [[ ! -z "$2" && "$2" != "s3://"* ]]; then
+        args=("$1" "s3://${2}")
+    else
+        args=("$1" "$2")
+    fi
+    s3_storage_perform "ls" "${args[@]}"
+}
+
+s3_storage_get() {
+    local args
+    if [[ "$2" != "s3://"* ]]; then
+        args=("$1" "s3://${2}" "$3")
+    else
+        args=("$1" "$2" "$3")
+    fi
+    s3_storage_perform "get" "${args[@]}"
+}
+
+s3_storage_put() {
+    local args
+    if [[ "$3" != "s3://"* ]]; then
+        args=("$1" "$2" "s3://${3}")
+    else
+        args=("$1" "$2" "$3")
+    fi
+    s3_storage_perform "put" "${args[@]}"
+}
+
+s3_storage_mkbucket() {
+    local args
+    if [[ "$2" != "s3://"* ]]; then
+        args=("$1" "s3://${2}")
+    else
+        args=("$1" "$2")
+    fi
+    s3_storage_perform "mb" "${args[@]}"
+}
+
+s3_storage_rmbucket() {
+    local args
+    if [[ "$2" != "s3://"* ]]; then
+        args=("$1" "s3://${2}")
+    else
+        args=("$1" "$2")
+    fi
+    s3_storage_perform "rb" "${args[@]}"
+}
+
+s3_storage_rm() {
+    local args
+    if [[ "$2" != "s3://"* ]]; then
+        args=("$1" "s3://${2}")
+    else
+        args=("$1" "$2")
+    fi
+    s3_storage_perform "del" "${args[@]}"
+}
